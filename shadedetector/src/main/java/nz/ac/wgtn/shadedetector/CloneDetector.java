b498b30780a145a1c130268baa5f04a9edab7a56
Jens Dietrich <jens.dietrich@gmail.com>
2023-03-10 15:15:01 +1300

    added abstraction to plugin in clone detectors

---
+++ shadedetector/src/main/java/nz/ac/wgtn/shadedetector/CloneDetector.java
@@ -0,0 +1,25 @@
+package nz.ac.wgtn.shadedetector;
+
+import java.io.File;
+
+/**
+ * Abstraction for a utility to detect the similarity of source code files.
+ * TODO: at the moment the intention is to use this to compare .java files, but this could be extended to .kotlin files etc
+ * @author jens dietrich
+ */
+public interface CloneDetector {
+
+    /**
+     * Returns a name that can be used to identify the tool. This is suitable to be used as argument in a CLI.
+     * @return
+     */
+    String getName() ;
+
+    /**
+     * Compare two source code files (.java). Return a similarity score between 0 (not similar) to 1 (very similar / identical).
+     * @param src1
+     * @param src2
+     * @return
+     */
+    double getSimilarityScore(File src1, File src2) ;
+}

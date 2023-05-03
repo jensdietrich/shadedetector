8a1a9d24b4ac0e23e3fd9821a4c2dc9bd77dd80e
Jens Dietrich <jens.dietrich@gmail.com>
2023-05-03 11:20:25 +0200

    added comment to blacklist

--- shadedetector/src/main/java/nz/ac/wgtn/shadedetector/Blacklist.java
+++ shadedetector/src/main/java/nz/ac/wgtn/shadedetector/Blacklist.java
@@ -8,10 +8,11 @@ import java.util.Set;
  * @author jens dietrich
  */
 public class Blacklist {
-    ;
 
     static boolean contains (Artifact artifact) {
         // outofmemory when extracting from jar -- check for zip bomb or similar
-        return artifact.getGroupId().equals("dev.dejvokep");
+        // might be related to https://bugs.openjdk.org/browse/JDK-7143743
+        // TODO investigate whether this only happends for certain JDK versions (11, 17 seems to be fine)
+        return artifact.getGroupId().equals("dev.dejvokep") ;
     }
 }

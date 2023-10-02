a4e952ba8c9b691d193876cae0cca95e286a992d
Tim White <walton.timothy.james.white@gmail.com>
2023-10-02 20:29:59 +1300

    Handle CVE-* subdirs for OWASP Dependency Check

--- sca/run-owasp-dependencycheck.sh
+++ sca/run-owasp-dependencycheck.sh
@@ -2,7 +2,7 @@
 
 # run owasp dependencycheck sca on all folders
 root=$(pwd)
-for d in ./*/ ; do
+for d in CVE-*/*/ ; do
   cd $d;
   echo "running owasp dependency check analysis on ${d}"
   dependency-check -scan . -f JSON -o scan-results/dependency-check -prettyPrint;

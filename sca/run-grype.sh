e96e6b719835b9a2dbe4df3ca82a1e2dd289259b
Tim White <walton.timothy.james.white@gmail.com>
2023-10-02 14:42:34 +1300

    Allow grype to use older DB files

--- sca/run-grype.sh
+++ sca/run-grype.sh
@@ -9,6 +9,7 @@ for d in ./*/ ; do
   # turn of all online activity
   export GRYPE_CHECK_FOR_APP_UPDATE=0
   export GRYPE_DB_AUTO_UPDATE=0
+  export GRYPE_DB_MAX_ALLOWED_BUILT_AGE=1000000h
   grype --output json --file scan-results/grype/grype-report.json .;
   cd $root;
 done

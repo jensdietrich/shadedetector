6dd5ecbdddaeeef0670a861599c9074e3fd23598
Tim White <walton.timothy.james.white@gmail.com>
2023-10-06 19:01:31 +1300

    Update modification time

--- tools/update_ghsa.sh
+++ tools/update_ghsa.sh
@@ -11,7 +11,8 @@ fi
 EXISTING_GHSA="$1"
 shift
 
+NOW=`date --iso-8601=seconds --utc | sed -e 's/+00:00/Z/'`
 ls -d "$@" |
 	perl -lne 's|.*/||; s|/$||; ($g, $a, $v) = split /__/; print "{\"name\": \"$g:$a\", \"versions\": [\"$v\"]}"' |
 	jq -s 'group_by(.name) | {"affected": [.[] | {"package": {"ecosystem": "Maven", "name": .[0].name}, "versions": [.[].versions[]]}]}' | 		# Group all versions with same groupId and artifactId
-	jq -j -s '.[0] + {"affected": (.[0].affected + .[1].affected)}' "$EXISTING_GHSA" -								# Merge into existing GHSA JSON
+	jq -j -s '.[0] + {"modified": "'"$NOW"'", "affected": (.[0].affected + .[1].affected)}' "$EXISTING_GHSA" -								# Merge into existing GHSA JSON

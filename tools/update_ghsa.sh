8bf4c0aad5470193fcacfc5237e7557d4bd63504
Tim White <walton.timothy.james.white@gmail.com>
2023-10-06 19:19:28 +1300

    Add reference to xshady-release URL for CVE

--- tools/update_ghsa.sh
+++ tools/update_ghsa.sh
@@ -11,8 +11,11 @@ fi
 EXISTING_GHSA="$1"
 shift
 
+# Remaining arguments are dirs that should all refer to the same CVE
+CVE="$(basename $(dirname $1))"
+
 NOW=`date --iso-8601=seconds --utc | sed -e 's/+00:00/Z/'`
 ls -d "$@" |
 	perl -lne 's|/$||; s|.*/||; ($g, $a, $v) = split /__/; print "{\"name\": \"$g:$a\", \"versions\": [\"$v\"]}"' |
 	jq -s 'group_by(.name) | {"affected": [.[] | {"package": {"ecosystem": "Maven", "name": .[0].name}, "versions": [.[].versions[]]}]}' | 		# Group all versions with same groupId and artifactId
-	jq -j -s '.[0] + {"modified": "'"$NOW"'", "affected": (.[0].affected + .[1].affected)}' "$EXISTING_GHSA" -								# Merge into existing GHSA JSON
+	jq -j -s '.[0] + {"modified": "'"$NOW"'", "affected": (.[0].affected + .[1].affected), "references": (.[0].references + [{"type": "WEB", "url": "https://github.com/jensdietrich/xshady-release/tree/main/'"$CVE"'"}])}' "$EXISTING_GHSA" -								# Merge into existing GHSA JSON

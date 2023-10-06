d89905562a8300bd61b63a3edd21ab1f0a43b0fe
Tim White <walton.timothy.james.white@gmail.com>
2023-10-06 18:31:47 +1300

    Grouping by GA works

--- tools/update_ghsa.sh
+++ tools/update_ghsa.sh
@@ -2,4 +2,4 @@
 
 EXISTING_GHSA="$1"
 shift
-ls -d "$@" | perl -lne 's|.*/||; ($g, $a, $v) = split /__/; print "{\"affected\": [{\"package\": {\"ecosystem\": \"Maven\", \"name\": \"$g:$a\"}, \"versions\": [\"$v\"]}]}"' | jq -s '.[0] + {"affected": (.[0].affected + .[1].affected)}' "$EXISTING_GHSA" -
+ls -d "$@" | perl -lne 's|.*/||; ($g, $a, $v) = split /__/; print "{\"name\": \"$g:$a\", \"versions\": [\"$v\"]}"' | jq -s 'group_by(.name) | {"affected": [.[] | {"package": {"ecosystem": "Maven", "name": .[0].name}, "versions": [.[].versions[]]}]}' | jq -j -s '.[0] + {"affected": (.[0].affected + .[1].affected)}' "$EXISTING_GHSA" -

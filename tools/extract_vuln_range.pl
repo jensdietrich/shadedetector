9ba8092cd1bfbe3c1c32384dc7c6726d1539f8c4
Tim White <walton.timothy.james.white@gmail.com>
2024-01-31 09:45:12 +1300

    Add logic to download maven-metadata.xml for each artifact and use its version ordering after sanity-checking it. Doesn't call it yet.

--- tools/extract_vuln_range.pl
+++ tools/extract_vuln_range.pl
@@ -3,6 +3,107 @@
 use strict;
 use warnings;
 
+# Call with two refs to arrays of numbers.
+sub compareNatural($$);		# Forward declaration
+sub compareNatural($$) {
+	my ($x, $y) = @_;
+	if (!@$x) {
+		return !@$y ? 0 : -1;
+	}
+
+	return 1 if !@$y;
+
+	die "Non-numeric component '$x->[0]' seen!" if $x->[0] !~ /\A\d+\z/;
+	die "Non-numeric component '$y->[0]' seen!" if $y->[0] !~ /\A\d+\z/;
+
+	if ($x->[0] == $y->[0]) {
+		shift @$x;
+		shift @$y;
+		return compareNatural($x, $y);
+	}
+
+	return $x->[0] <=> $y->[0];
+}
+
+sub extractSimpleVersionArray($) {
+	my ($prefix) = $_[0] =~ /^([0-9.]+)/ or die "Could not extract a simple version prefix from '$_[0]'!";
+	return map { $_ + 0 } split /\./, $prefix;
+}
+
+sub getMavenMetadataXmlUrlFor($$) {
+	my ($g, $a) = @_;
+	$g =~ tr|.|/|;
+	return "https://repo1.maven.org/maven2/$g/$a/maven-metadata.xml";
+}
+
+
+# Heuristically check that maven-metadata.xml versions "look right", i.e., look like they're in the correct order, and die if not
+sub checkMavenMetadataVersionsAreSensiblyOrdered($) {
+	my ($mapVersionToPosition) = @_;
+	my @original;
+	foreach (each %$mapVersionToPosition) {
+		$original[$_->[0]] = extractSimpleVersionArray($_->[1]);
+	}
+
+	my @sorted = sort compareNatural @original;
+
+	for (my $i = 0; $i < @original; ++$i) {
+		die "Missing version info for position $i!" if !defined $original[$i];
+		my $o = join(".", @{$original[$i]});
+		my $s = join(".", @{$sorted[$i]});
+		die "Expected version #$i to be '$s' but saw '$o'!" if $o ne $s;
+	}
+}
+
+sub downloadMavenMetadataXml($$) {
+	my ($g, $a) = @_;
+	my $url = getMavenMetadataXmlUrlFor($g, $a);
+
+	#my @versions;
+	my %mapVersionToPosition;
+	my $i = 0;
+	local $_;
+	print STDERR "Downloading metadata $url for $g:$a...\n";		#DEBUG
+	foreach (`curl $url`) {
+		if (m|<version>(.*)</version>\s*$|) {
+			#push @versions, $1;
+			$mapVersionToPosition{$1} = $i++;
+		}
+	}
+	print STDERR "Extracted metadata on $i versions from $url for $g:$a...\n";		#DEBUG
+
+	checkMavenMetadataVersionsAreSensiblyOrdered(\%mapVersionToPosition);		# Dies if it "looks wrong"
+
+	#return @versions;
+	return \%mapVersionToPosition;
+}
+
+my %mavenMetadataXmlCache = ();
+sub getMavenMetadataXml($$) {
+	my ($g, $a) = @_;
+	if (!exists $mavenMetadataXmlCache{"$g:$a"}) {
+		$mavenMetadataXmlCache{"$g:$a"} = downloadMavenMetadataXml($g, $a);
+	}
+
+	return $mavenMetadataXmlCache{"$g:$a"};
+}
+
+sub positionInVersionList($$$) {
+	my ($g, $a, $v) = @_;
+	my $mapVersionToPosition = getMavenMetadataXml($g, $a);
+	die "Could not find Maven metadata for $g:$a!" if !defined $mapVersionToPosition;
+	my $pos = $mapVersionToPosition->{$v};
+	die "Could not find version $v in Maven metadata for $g:$a!" if !defined $pos;
+
+	return $pos;
+}
+
+sub compareByMavenMetadata($$$$) {
+	my ($g, $a, $v1, $v2) = @_;
+	return positionInVersionList($g, $a, $v1) <=> positionInVersionList($g, $a, $v2);
+}
+
+# Main program
 while (<>) {
 	if (my ($cve, $g, $a, $v, $result) = m!^.* tests in \S+/([^/]+)/(\S+?)__(\S+?)__(\S+?): .* -> vuln is (present|absent)$!) {
 		print join("\t", $cve, "$g:$a", $v, $result), "\n";
